747e30b02f8f8ad5c082143ad4e5c29bbf10e15b
Fatiha-A <145744145+Fatiha-A@users.noreply.github.com>
2025-02-07 15:28:03 -0400

    Create Anton and Danik.py

---
+++ Anton and Danik.py
@@ -0,0 +1,8 @@
+loop = int(input())
+string = input()
+if string.count('A') > string.count('D'):
+    print('Anton')
+elif string.count('A') < string.count('D'):
+    print('Danik')
+else:
+    print('Friendship')
