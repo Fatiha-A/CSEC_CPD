3799eff38eb3654b71e5e3b998f011481681da40
Fatiha-A <145744145+Fatiha-A@users.noreply.github.com>
2025-02-12 09:39:51 -0400

    Create Die Roll.py

---
+++ Die Roll.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+highest = max(a, b)
+favorable_outcomes = 6 - highest + 1
+total_outcomes = 6
+for i in [2, 3]:
+    while favorable_outcomes % i == 0 and total_outcomes % i == 0:
+        favorable_outcomes //= i
+        total_outcomes //= i
+
+print(f"{favorable_outcomes}/{total_outcomes}")
+
+Colorful Stones (Simplified Edition)
